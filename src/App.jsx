685d5eccc5c200293c7256b56d1734d0e5e1f9a8
Mamun-Miah <mahadyhasen7@gmail.com>
2024-07-12 10:34:41 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-6xl text-purple-600'>Hello testdsd </h1>
+    </>
+  )
+}
+
+export default App
